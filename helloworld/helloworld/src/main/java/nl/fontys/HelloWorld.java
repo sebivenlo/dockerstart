0652b1d9ca58a43002d13d08841681967ab658dc
pieter <p.vandenhombergh@fontys.nl>
2017-09-06 11:06:25 +0200

    simplest form

--- helloworld/helloworld/src/main/java/nl/fontys/HelloWorld.java
+++ helloworld/helloworld/src/main/java/nl/fontys/HelloWorld.java
@@ -2,7 +2,6 @@ package nl.fontys;
 
 import java.io.Serializable;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -20,62 +19,25 @@ import javax.sql.DataSource;
 @SessionScoped
 public class HelloWorld implements Serializable {
 
-    private static final String DB_HOST = "app-db";
-    private static final int DB_PORT = 5433;
-    private static final String DB_NAME = "docker";
-    private static final String DB_USER = "docker";
-    private static final String DB_PASS = "docker";
-
     private static final long serialVersionUID = 1L;
-    @Resource( lookup = "java:jboss/jdbc/dockerds" )
+    @Resource( lookup = "java:/jdbc/dockerds" )
     DataSource ds;
 
     public List<String> getMessages() {
-        //return Arrays.asList("Hello", "World");
-        ResultSet rs;
 
         String query = "SELECT * FROM awesomeTable";
         List<String> records = new ArrayList<>();
-        records.add( "Hi there too" );
-        records.add( ds.toString() );
-        Connection con = null;
-        try {
-            con = ds.getConnection();
-            records.add( "con=" + con.toString() );
-        } catch ( SQLException ex ) {
-            Logger.getLogger( HelloWorld.class.getName() ).log( Level.SEVERE, null, ex );
-            
-            throw new RuntimeException( ex );
-        }
-        try (
+        try ( Connection con = ds.getConnection();
                 PreparedStatement ps = con.prepareStatement( query ); ) {
             ps.execute();
-            rs = ps.getResultSet();
-
-            while ( rs.next() ) {
-                records.add( rs.getString( 2 ) );
+            try ( ResultSet rs = ps.getResultSet(); ) {
+                while ( rs.next() ) {
+                    records.add( rs.getString( 2 ) );
+                }
             }
-            records.add( "done" );
-        } catch ( SQLException e ) {
-
-            throw new RuntimeException(e);
+        } catch ( SQLException ex ) {
+            Logger.getLogger( HelloWorld.class.getName() ).log( Level.SEVERE, null, ex );
         }
         return records;
-    }
-
-    public Connection getConnection() throws SQLException {
-        Connection con = null;
-        String url = "jdbc:postgresql://" + DB_HOST + ":" + DB_PORT + "/" + DB_NAME;
-
-        try {
-            System.out.println( "Trying to connect to database: " + url );
-            Class.forName( "org.postgresql.Driver" );
-            con = DriverManager.getConnection( url, DB_USER, DB_PASS );
-            System.out.println( "Connection completed." );
-            return con;
-        } catch ( ClassNotFoundException cnf ) {
-            System.out.println( "Failed to load database library." );
-        }
-        return con;
     }
 }
